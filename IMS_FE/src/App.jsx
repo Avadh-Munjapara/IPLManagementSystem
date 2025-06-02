ee193414f8325c19d3352afa0e7d984013885075
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2025-06-02 18:36:24 +0530

    Refactor App component to render Login page and add .gitignore for node_modules

--- IMS_FE/src/App.jsx
+++ IMS_FE/src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import 
-
+import React from 'react';
+import Login from './pages/Login';
 const App = () => {
-  const appRouter = cre
+
   return (
-    <div>App</div>
+<Login/>
   )
 }
 
