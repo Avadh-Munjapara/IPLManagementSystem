432f2620834cd44988d8f1748c4acd7004e24ae3
romit patel <patelromit6952@gmail.com>
2025-06-02 18:37:17 +0530

    Login and Regester Page

--- IMS_FE/src/App.jsx
+++ IMS_FE/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import 
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+import Toaster from 'react-hot-toast'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 const App = () => {
-  const appRouter = cre
+  const  myRouter = createBrowserRouter([
+   {path:'/',element:<Register/>}
+  ])
   return (
-    <div>App</div>
+    <div>
+      <RouterProvider router={myRouter}/>
+      <Toaster/>
+    </div>
   )
 }
 
