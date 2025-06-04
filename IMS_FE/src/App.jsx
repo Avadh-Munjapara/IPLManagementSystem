a73c90021edd95413e67714640fad17400bf956a
romit patel <patelromit6952@gmail.com>
2025-06-04 12:39:11 +0530

    login page

--- IMS_FE/src/App.jsx
+++ IMS_FE/src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import Login from './pages/Login';
+import React from "react";
+import Login from "./pages/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";  
 const App = () => {
 
-  return (
-<Login/>
-  )
-}
+  const myRouter = createBrowserRouter(
+    [
+  {path: "/" ,element: <Login />},
 
-export default App
+  ]);
+  return (
+    <div>
+   <RouterProvider router={myRouter}/>
+    <Toaster/>
+    </div>
+  );
+};
+
+export default App;
